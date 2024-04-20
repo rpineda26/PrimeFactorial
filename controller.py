9c4e898955a963aff6ec8ad4e5d063e58408cf92
rpineda26 <91407317+rpineda26@users.noreply.github.com>
2024-04-20 17:41:13 +0800

    Doc: refine documentation in controller methods

--- controller.py
+++ controller.py
@@ -15,39 +15,30 @@ from math import sqrt
 
 
 """
-@definition: This function checks if a number is prime or not.
+@definition: This function checks if a number is prime or not. Time Complexity: O(n)
 @params:  num  -  the number to be checked
 @returns: True  - if the number is prime
           False - if the number is not prime
 """
-def isPrime(num):
+def naive_isPrime(num):
     if num > 1:
         for i in range(2,num):
             if (num % i) == 0:
-                print(num,"is not a prime number")
                 return False
         else:
             return True
-    else:
+    else: # 0 and 1 are not prime numbers
         return False
+    
+
 """
-@definition: This function computes the factorial of a number.
+@definition: This function computes the factorial of a number using a recursive approach. Time Complexity: O(n)
 @params:  num  -  the number to be computed
 @returns: 1 - base case when num is 0
         num * factorial(num-1) - recursive case
 """
-def factorial(num):
-    if num == 0:
+def naive_factorial(num):
+    if num == 0: #base case
         return 1
     else:
-        return num * factorial(num-1)
-
-
-
-def displayAction(state, input, string, head):
-    print("Current State: "+state)
-    print("Current Input: "+input)
-    print("Word Input: " +string)
-    print("head: "+head)
-
-    main()
+        return num * naive_factorial(num-1) #recursive case
